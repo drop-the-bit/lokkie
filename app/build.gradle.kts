3b70dff188af064286af6f50b737cb19c86345eb
YlongCha <152164343+leocha814@users.noreply.github.com>
2025-02-21 02:45:09 +0900

    webview 추가
    Fragment 실패

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
     implementation(libs.play.services.ads)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/lokkie/yrapp/MainActivity.kt
+++ app/src/main/java/com/lokkie/yrapp/MainActivity.kt
@@ -1,34 +1,27 @@
 package com.lokkie.yrapp
 
 import android.os.Bundle
-import android.widget.Toast
+import android.text.TextUtils.replace
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.Fragment
 import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdView
 import com.lokkie.yrapp.databinding.ActivityMainBinding
 import com.lokkie.yrapp.ui.theme.YrAppTheme
-
-private const val TOAST_TEXT = "Test ads are being shown. " +
-        "To show live ads, replace the ad unit ID in res/values/strings.xml " +
-        "with your own ad unit ID."
 
 
 class MainActivity : ComponentActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -36,8 +29,21 @@ class MainActivity : ComponentActivity() {
             .setRequestAgent("android_studio:ad_template")
             .build()
         binding.adView.loadAd(adRequest)
-        Toast.makeText(this, TOAST_TEXT, Toast.LENGTH_LONG).show()
+
+        val myWebView: WebView = binding.root.findViewById(R.id.webview)
+
+        myWebView.settings.run {
+            // 웹뷰 자바스크립트 허용
+            javaScriptEnabled = true
+            javaScriptCanOpenWindowsAutomatically = true
+            setSupportMultipleWindows(true)
+        }
+        myWebView.webViewClient = WebViewClient()
+        myWebView.webChromeClient = WebChromeClient()
+        myWebView.loadUrl("https://www.google.com")
+
     }
+
 }
 
 @Composable
